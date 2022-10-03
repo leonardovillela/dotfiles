cf8e56b1b0c09eb2de81f8fbcb63ffa006e05af0
Leonardo Villela <leonardo.villela37@gmail.com>
2022-10-04 01:12:49 +0200

    Refactor everything to use chezmoi

---
+++ bootstrap.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -eufo pipefail
+
+echo ""
+echo "🤚  This script will setup .dotfiles for you."
+read -n 1 -r -s -p $'    Press any key to continue or Ctrl+C to abort...\n\n'
+  
+if [ ! "$(command -v chezmoi)" ]; then
+  echo "Installing: chezmoi ********************"
+  bin_dir="$HOME/.local/bin"
+  chezmoi="$bin_dir/chezmoi"
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsSL chezmoi.io/get)" -- -b "$bin_dir"
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- chezmoi.io/get)" -- -b "$bin_dir"
+  else
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
+  fi
+else
+  chezmoi=chezmoi
+fi
+
+# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+# exec: replace current process with chezmoi init
+exec "$chezmoi" init --apply "--source=$script_dir"
---
+++ dot_aliases.sh
@@ -0,0 +1,6 @@
+alias ggpushf="ggpush --force-with-lease"
+alias brewup="brew update; brew upgrade; brew cleanup; brew doctor"
+alias {ZSHRC, zshrc}="$EDITOR ~/.zshrc"
+alias cat="bat --paging=never"
+alias vim="nvim"
+alias cz=chezmoi

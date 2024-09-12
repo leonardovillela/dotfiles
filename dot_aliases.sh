43a083fd35fbccccb4371f6222c38ae54e07e6f0
Leonardo Villela <leonardo.villela37@gmail.com>
2024-09-13 01:46:16 +0200

    back to using chezmoi to manage my dotfiles

--- dot_aliases.sh
+++ dot_aliases.sh
@@ -2,5 +2,8 @@ alias ggpushf="ggpush --force-with-lease"
 alias brewup="brew update; brew upgrade; brew cleanup; brew doctor"
 alias {ZSHRC, zshrc}="$EDITOR ~/.zshrc"
 alias cat="bat --paging=never"
+alias catp="bat"
 alias vim="nvim"
 alias cz=chezmoi
+alias ghcs="gh copilot suggest"
+alias ghce="gh copilot explain"

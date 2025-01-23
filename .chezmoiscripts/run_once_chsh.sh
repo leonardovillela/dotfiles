11159fe2b535f170daf6f800fc2170d409d7f329
Leonardo Villela <leonardo.villela37@gmail.com>
2025-01-23 02:13:20 +0100

    I think I finally managed to make this work for codespaces in the correct way

--- .chezmoiscripts/run_once_chsh.sh
+++ .chezmoiscripts/run_once_chsh.sh
@@ -3,20 +3,15 @@
 if [ "$(command -v zsh)" ]; then
   echo Zsh is available, but not necessarily the default shell.
 
-  if [ $CODESPACES ]; then
-    echo Running on codespaces. Changing shell to zsh.
-    sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
+  # What shell is the user currently using?
+  if [ "$(uname)" = "Darwin" ]; then
+    myshell=$(basename "$(dscl . -read "${HOME}" UserShell | sed 's/UserShell: //')")
   else
-    # What shell is the user currently using?
-    if [ "$(uname)" = "Darwin" ]; then
-      myshell=$(basename "$(dscl . -read "${HOME}" UserShell | sed 's/UserShell: //')")
-    else
-      myshell=$(basename "$(grep """$(whoami)""" /etc/passwd | cut -f 7 -d :)")
-    fi
-    if [ "${myshell}" != "zsh" ]; then
-      # Set zsh as default shell.
-      echo Running sudo to change your shell to zsh.
-      sudo chsh -s "$(which zsh)" "$(whoami)"
-    fi
+    myshell=$(basename "$(grep """$(whoami)""" /etc/passwd | cut -f 7 -d :)")
+  fi
+  if [ "${myshell}" != "zsh" ]; then
+    # Set zsh as default shell.
+    echo Running sudo to change your shell to zsh.
+    sudo chsh -s "$(which zsh)" "$(whoami)"
   fi
 fi
